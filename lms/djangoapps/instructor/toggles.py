74d8953408dfbdcaa868094a1f1dde8af70b431f
Ahtisham Shahid <ahtisham300@gmail.com>
2020-07-08 14:14:53 +0500

    Removed extra js and updated comments

--- lms/djangoapps/instructor/toggles.py
+++ lms/djangoapps/instructor/toggles.py
@@ -6,6 +6,18 @@ from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag, WaffleFlagNam
 
 WAFFLE_NAMESPACE = 'instructor'
 
+# Waffle flag enable new data download UI on specific course.
+# .. toggle_name: instructor.enable_data_download_v2
+# .. toggle_implementation: WaffleFlag
+# .. toggle_default: False
+# .. toggle_description: instructor
+# .. toggle_category: Instructor dashboard
+# .. toggle_use_cases: incremental_release, open_edx
+# .. toggle_creation_date: 2020-07-8
+# .. toggle_expiration_date: ??
+# .. toggle_warnings: ??
+# .. toggle_tickets: PROD-
+# .. toggle_status: supported
 DATA_DOWNLOAD_V2 = CourseWaffleFlag(
     waffle_namespace=WaffleFlagNamespace(name=WAFFLE_NAMESPACE, log_prefix='instructor_dashboard: '),
     flag_name='enable_data_download_v2',
